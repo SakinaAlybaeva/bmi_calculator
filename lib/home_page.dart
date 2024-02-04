84a0e6e3f0f5a9ac46c0fe946e340db832eb7119
SakinaAlybaeva <alybaevakina@gmail.com>
2024-02-04 18:02:43 +0300

    BMI Calculator

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+Color activeColor = Colors.pink;
+Color inacitveColor = const Color(0xff0A001F).withOpacity(0.95);
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -7,18 +10,31 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-double cm = 150;
-
 class _HomePageState extends State<HomePage> {
+  Color maleColor = inacitveColor;
+  Color femaleColor = inacitveColor;
+  double heightValue = 155;
+
+  void changeGenderColor(int san) {
+    if (san == 1) {
+      maleColor = activeColor;
+      femaleColor = inacitveColor;
+    } else if (san == 2) {
+      femaleColor = activeColor;
+      maleColor = inacitveColor;
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff0A001F).withOpacity(0.9),
+      backgroundColor: const Color.fromARGB(255, 22, 7, 54).withOpacity(0.90),
       appBar: AppBar(
-        backgroundColor: Color(0xff0A001F).withOpacity(0.9),
+        backgroundColor: const Color.fromARGB(255, 22, 7, 54).withOpacity(0.90),
         title: const Center(
           child: Text(
-            'Tapshyrma 8',
+            'BMI CALCULATOR',
             style: TextStyle(color: Colors.white),
           ),
         ),
@@ -26,119 +42,107 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(
+          const SizedBox(
             height: 40,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.2,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blue,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.male,
-                      size: 50,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Male',
-                      style: TextStyle(fontSize: 30, color: Colors.white),
-                    ),
-                  ],
-                ),
+              GenderWidget(
+                icon: Icons.male,
+                text: 'Male',
+                onTap: () {
+                  changeGenderColor(1);
+                },
+                tus: maleColor,
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.2,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blue,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.female,
-                      size: 50,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Female',
-                      style: TextStyle(fontSize: 30, color: Colors.white),
-                    ),
-                  ],
-                ),
+              GenderWidget(
+                icon: Icons.female,
+                text: 'Female',
+                onTap: () {
+                  changeGenderColor(2);
+                },
+                tus: femaleColor,
               ),
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Container(
-            width: MediaQuery.of(context).size.width * 0.9,
+            width: MediaQuery.of(context).size.width * 0.85,
             decoration: BoxDecoration(
-              color: Colors.blue,
+              color: const Color(0xff0A001F).withOpacity(0.95),
               borderRadius: BorderRadius.circular(10),
             ),
             child: Column(
               children: [
                 Text(
-                  'Height',
-                  style: TextStyle(color: Colors.white, fontSize: 30),
+                  'Height'.toUpperCase(),
+                  style: const TextStyle(color: Colors.white, fontSize: 30),
                 ),
-                Text(
-                  '$cm cm',
-                  style: TextStyle(color: Colors.white, fontSize: 40),
+                Row(
+                  textBaseline: TextBaseline.alphabetic,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      heightValue.toStringAsFixed(0),
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      'cm',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
                 ),
                 Slider(
-                  value: cm,
-                  onChanged: (at) {
+                  value: heightValue,
+                  onChanged: (value) {
                     setState(() {
-                      cm = at;
+                      heightValue = value;
                     });
                   },
-                  min: 150,
-                  max: 195,
-                  divisions: 50,
+                  max: 220,
+                  min: 55,
                 ),
               ],
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 40,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
-                width: MediaQuery.of(context).size.width * 0.3,
+                width: MediaQuery.of(context).size.width * 0.4,
                 height: MediaQuery.of(context).size.height * 0.2,
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: const Color(0xff0A001F).withOpacity(0.95),
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Text(
-                      'Weight',
-                      style: TextStyle(
+                      'Weight'.toUpperCase(),
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 22,
                       ),
                     ),
-                    Text(
+                    const Text(
                       '60',
                       style: TextStyle(color: Colors.white, fontSize: 30),
                     ),
-                    Row(
+                    const Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Icon(
@@ -157,27 +161,27 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               Container(
-                width: MediaQuery.of(context).size.width * 0.3,
+                width: MediaQuery.of(context).size.width * 0.4,
                 height: MediaQuery.of(context).size.height * 0.2,
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: const Color(0xff0A001F).withOpacity(0.95),
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Text(
-                      'Age',
-                      style: TextStyle(
+                      'Age'.toUpperCase(),
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 22,
                       ),
                     ),
-                    Text(
+                    const Text(
                       '28',
                       style: TextStyle(color: Colors.white, fontSize: 30),
                     ),
-                    Row(
+                    const Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Icon(
@@ -200,15 +204,63 @@ class _HomePageState extends State<HomePage> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: 40,
-            color: Colors.pink,
-            child: Center(
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
               child: Text(
-                'Calculator',
-                style: TextStyle(color: Colors.white, fontSize: 25),
+                'CALCULATOR',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class GenderWidget extends StatelessWidget {
+  const GenderWidget({
+    super.key,
+    this.icon,
+    this.text,
+    this.onTap,
+    this.tus,
+  });
+  final IconData? icon;
+  final String? text;
+  final Function()? onTap;
+  final Color? tus;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.4,
+        height: MediaQuery.of(context).size.height * 0.2,
+        decoration: BoxDecoration(
+          color: tus,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 60,
+              color: Colors.white,
+            ),
+            Text(
+              text!.toUpperCase(),
+              style: const TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
